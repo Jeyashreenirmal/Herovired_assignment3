94f152bd7da686b2e935f36fdf9352ad9fc24704
Jeyashreenirmal <jsree261997@gmail.com>
2025-10-25 22:14:54 +0200

    shell script for Apache server backup

---
+++ ApacheBackup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Variables
+BACKUP_DIR="/root/backups"
+DATE=$(date +%F)
+SERVER_NAME="apache"
+CONFIG_DIR="/etc/httpd"
+DOC_ROOT="/var/www/html"
+BACKUP_FILE="$BACKUP_DIR/${SERVER_NAME}_backup_$DATE.tar.gz"
+
+# Create backup
+tar -czf $BACKUP_FILE $CONFIG_DIR $DOC_ROOT
+
+# Verify backup integrity
+echo "Verifying $BACKUP_FILE..."
+tar -tzf $BACKUP_FILE
